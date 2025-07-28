0dfdc4738008d70048c46e0a25086bf8820d00c8
dbautista9015 <dbautista9015@gmail.com>
2025-07-27 20:34:00 -0700

    Add remaps for yanking, scrolling and searching

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Remap for grabbing 1 or more lines and move them as a unit
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Remap for scrolling half a page up and down, then centering screen
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Remap for searching a term and once go to the next term, it centers screen
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Remap for when yanking a word to have that word available for paste
+map("x", "<leader>p", '"_dP')
